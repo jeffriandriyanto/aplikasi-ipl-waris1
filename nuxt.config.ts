4f768f23d3a83e6f8856e289729f8d9a33e922bc
Jeffri Andriyanto <jeff.tm07@gmail.com>
2026-05-26 12:37:18 +0700

    Fix env

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,12 +16,13 @@ export default defineNuxtConfig({
   runtimeConfig: {
     firebaseServiceAccount: process.env.FIREBASE_SERVICE_ACCOUNT || '',
     public: {
-      firebaseApiKey: '',
-      firebaseAuthDomain: '',
-      firebaseProjectId: '',
-      firebaseStorageBucket: '',
-      firebaseMessagingSenderId: '',
-      firebaseAppId: '',
+      firebaseApiKey: process.env.NUXT_PUBLIC_FIREBASE_API_KEY || '',
+      firebaseAuthDomain: process.env.NUXT_PUBLIC_FIREBASE_AUTH_DOMAIN || '',
+      firebaseProjectId: process.env.NUXT_PUBLIC_FIREBASE_PROJECT_ID || '',
+      firebaseAppId: process.env.NUXT_PUBLIC_FIREBASE_APP_ID || '',
+      
+      firebaseStorageBucket: process.env.NUXT_PUBLIC_FIREBASE_STORAGE_BUCKET || '',
+      firebaseMessagingSenderId: process.env.NUXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || '',
     },
   },
 
